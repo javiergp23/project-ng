97162fdb7ce08bef29de578a28660d01a3b11e3f
javiergp23 <javgon2306@gmail.com>
2026-04-08 23:01:11 -0300

    formControl

---
+++ src/app/components/form-control/formulario.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [ReactiveFormsModule],
+  templateUrl: './form-control.html',
+  styleUrl: './form-control.css',
+})
+export class Formulario {
+  movieForm: FormGroup;
+  name: FormControl;
+  duration: FormControl;
+  director: FormControl;
+
+  constructor(){
+    this.name = new FormControl();
+    this.duration = new FormControl();
+    this.director = new FormControl();
+    this.movieForm = new FormGroup({
+      name: this.name,
+      duration: this.duration,
+      director: this.director
+    });
+  }
+}
