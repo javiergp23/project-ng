f1460be55ddbe997682bc36a4a29c74184f79f5f
javiergp23 <javgon2306@gmail.com>
2026-04-22 22:13:38 -0300

    onInit Service

--- src/app/pages/details/details.ts
+++ src/app/pages/details/details.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header } from "../../components/header/header";
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../services/movieService';
@@ -10,15 +10,19 @@ import Movie from '../../models/Movie';
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
-export class Details {
+export class Details implements OnInit {
 
   selectedMovie?: Movie;
 
 
   constructor(private route: ActivatedRoute, private movieService: MovieService){
+
+  }
+
+  ngOnInit(): void {
     const movieName = decodeURIComponent(this.route.snapshot.params['movieName']);
     console.log(movieName);
     this.selectedMovie = this.movieService.getMovie(movieName);
-    movieService.getMovie(movieName);
+      this.movieService.getMovie(movieName);
   }
 }
