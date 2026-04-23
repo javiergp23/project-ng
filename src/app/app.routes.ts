20e4868528fbfdb579abfd7f2ac095a3d35d293f
javiergp23 <javgon2306@gmail.com>
2026-04-22 22:31:21 -0300

    add data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './pages/home/home';
 import { MovieList } from './pages/movie-list/movie-list';
 import { MovieForm } from './pages/movie-form/movie-form';
 import { Details } from './pages/details/details';
+import { Users } from './pages/users/users';
 
 export const routes: Routes = [
   {path: '', component: Home},
   {path: 'movies', component: MovieList},
   {path: 'create', component: MovieForm},
-  {path: 'movie/:movieName', component: Details}
+  {path: 'movie/:movieName', component: Details},
+  {path: 'user', component: Users}
 ];
